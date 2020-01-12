9144db8b5f84cc085e348c45a3cca4d72c7b652c
bugra <bgraknd@gmail.com>
2020-01-12 11:05:18 +0300

    Add retrofit classes

---
+++ app/src/main/java/com/bugra/movieapp/MovieApiService.kt
@@ -0,0 +1,12 @@
+package com.bugra.movieapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApiService {
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key") apiKey: String
+    ): Call<List<PopularMovies>>
+}
---
+++ app/src/main/java/com/bugra/movieapp/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package com.bugra.movieapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient {
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BuildConfig.API_URL)
+        .build()
+
+    private val apiService = retrofit.create(MovieApiService::class.java)
+
+    fun getMovieService(): MovieApiService = apiService
+}
